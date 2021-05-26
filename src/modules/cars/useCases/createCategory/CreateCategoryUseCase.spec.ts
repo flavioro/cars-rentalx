00039d4b230c5a40b016ec1f18b48eb2a1b706a0
flavioro <flavioro@gmail.com>
2021-05-26 15:26:22 -0300

    Inicialize test with jest, need implementation test

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,9 @@
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase"
+
+
+describe("Create category", () => {
+
+  it("should be able to create a new category", () => {
+    const createCategory = new CreateCategoryUseCase()
+  })
+})
